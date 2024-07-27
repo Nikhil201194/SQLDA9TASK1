79f959313520851718316fa0e12e45059e184f61
Nikhil201194 <ngondane5@gmail.com>
2024-07-27 03:53:45 -0700

    Add files via upload

---
+++ SQL student data Task 1.sql
@@ -0,0 +1,119 @@
+create table student(
+	id serial primary key,
+	first_name varchar,
+	gender varchar,
+	age int,
+	result varchar
+)
+
+select * from student
+
+insert into student (id, first_name, gender, age, result) values (1, 'Alexander', 'female', 25, 'fail')
+
+select * from student
+
+insert into student (id, first_name, gender, age, result) values (2, 'Aron', 'male', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (3, 'Morena', 'female', 20, 'fail');
+insert into student (id, first_name, gender, age, result) values (4, 'Gerhardt', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (5, 'Iris', 'male', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (6, 'Gianina', 'female', 21, 'fail');
+insert into student (id, first_name, gender, age, result) values (7, 'Davin', 'male', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (8, 'Wynn', 'male', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (9, 'Gloriana', 'female', 25, 'pass');
+insert into student (id, first_name, gender, age, result) values (10, 'Shandee', 'male', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (11, 'Redd', 'male', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (12, 'Adamo', 'male', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (13, 'Allegra', 'male', 23, 'pass');
+insert into student (id, first_name, gender, age, result) values (14, 'Hans', 'female', 23, 'pass');
+insert into student (id, first_name, gender, age, result) values (15, 'Konstance', 'male', 21, 'fail');
+insert into student (id, first_name, gender, age, result) values (16, 'Denney', 'female', 22, 'pass');
+insert into student (id, first_name, gender, age, result) values (17, 'Artur', 'male', 20, 'pass');
+insert into student (id, first_name, gender, age, result) values (18, 'Ethelred', 'male', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (19, 'Genny', 'female', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (20, 'Taber', 'female', 24, 'pass');
+insert into student (id, first_name, gender, age, result) values (21, 'Magdalene', 'male', 22, 'pass');
+insert into student (id, first_name, gender, age, result) values (22, 'Georgina', 'male', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (23, 'Roarke', 'female', 25, 'pass');
+insert into student (id, first_name, gender, age, result) values (24, 'Jacquetta', 'female', 20, 'fail');
+insert into student (id, first_name, gender, age, result) values (25, 'Cullen', 'male', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (26, 'Georgetta', 'female', 22, 'pass');
+insert into student (id, first_name, gender, age, result) values (27, 'Georas', 'female', 24, 'pass');
+insert into student (id, first_name, gender, age, result) values (28, 'Elyssa', 'female', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (29, 'Kennett', 'female', 20, 'pass');
+insert into student (id, first_name, gender, age, result) values (30, 'Skylar', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (31, 'Kaia', 'female', 23, 'pass');
+insert into student (id, first_name, gender, age, result) values (32, 'Booth', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (33, 'Carey', 'female', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (34, 'Dale', 'female', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (35, 'Faunie', 'male', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (36, 'Cosimo', 'male', 20, 'pass');
+insert into student (id, first_name, gender, age, result) values (37, 'Inez', 'male', 25, 'pass');
+insert into student (id, first_name, gender, age, result) values (38, 'Kesley', 'female', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (39, 'Adah', 'female', 24, 'pass');
+insert into student (id, first_name, gender, age, result) values (40, 'Enrichetta', 'male', 24, 'pass');
+insert into student (id, first_name, gender, age, result) values (41, 'Horst', 'female', 20, 'fail');
+insert into student (id, first_name, gender, age, result) values (42, 'Harlene', 'female', 20, 'pass');
+insert into student (id, first_name, gender, age, result) values (43, 'Bonny', 'female', 20, 'pass');
+insert into student (id, first_name, gender, age, result) values (44, 'Kalie', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (45, 'Angeline', 'male', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (46, 'Ilaire', 'female', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (47, 'Noah', 'male', 20, 'fail');
+insert into student (id, first_name, gender, age, result) values (48, 'Janelle', 'male', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (49, 'Gregorius', 'female', 22, 'pass');
+insert into student (id, first_name, gender, age, result) values (50, 'Max', 'male', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (51, 'Alverta', 'male', 23, 'pass');
+insert into student (id, first_name, gender, age, result) values (52, 'Suzi', 'female', 21, 'fail');
+insert into student (id, first_name, gender, age, result) values (53, 'Donnie', 'female', 20, 'fail');
+insert into student (id, first_name, gender, age, result) values (54, 'Jaime', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (55, 'Bevin', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (56, 'Berkeley', 'male', 22, 'pass');
+insert into student (id, first_name, gender, age, result) values (57, 'Pavlov', 'male', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (58, 'Faulkner', 'male', 22, 'pass');
+insert into student (id, first_name, gender, age, result) values (59, 'Weider', 'male', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (60, 'Martelle', 'male', 23, 'pass');
+insert into student (id, first_name, gender, age, result) values (61, 'Keelia', 'female', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (62, 'Sondra', 'female', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (63, 'Coraline', 'female', 24, 'pass');
+insert into student (id, first_name, gender, age, result) values (64, 'Vince', 'male', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (65, 'Desmund', 'female', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (66, 'Lil', 'female', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (67, 'Wendeline', 'female', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (68, 'Shelby', 'female', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (69, 'Clayborn', 'female', 24, 'pass');
+insert into student (id, first_name, gender, age, result) values (70, 'Marieann', 'male', 21, 'pass');
+insert into student (id, first_name, gender, age, result) values (71, 'Ursola', 'male', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (72, 'Orv', 'male', 25, 'pass');
+insert into student (id, first_name, gender, age, result) values (73, 'Osborn', 'male', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (74, 'Gloriana', 'female', 25, 'pass');
+insert into student (id, first_name, gender, age, result) values (75, 'Joya', 'male', 22, 'pass');
+insert into student (id, first_name, gender, age, result) values (76, 'Alwin', 'female', 25, 'pass');
+insert into student (id, first_name, gender, age, result) values (77, 'Lemmy', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (78, 'Benton', 'female', 24, 'pass');
+insert into student (id, first_name, gender, age, result) values (79, 'Carlota', 'female', 21, 'fail');
+insert into student (id, first_name, gender, age, result) values (80, 'Arvin', 'male', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (81, 'Lorita', 'male', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (82, 'Adoree', 'male', 23, 'pass');
+insert into student (id, first_name, gender, age, result) values (83, 'Carma', 'male', 23, 'pass');
+insert into student (id, first_name, gender, age, result) values (84, 'Brittani', 'female', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (85, 'Obidiah', 'female', 20, 'fail');
+insert into student (id, first_name, gender, age, result) values (86, 'Tabatha', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (87, 'Birk', 'male', 21, 'fail');
+insert into student (id, first_name, gender, age, result) values (88, 'Richard', 'female', 23, 'pass');
+insert into student (id, first_name, gender, age, result) values (89, 'Gordy', 'female', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (90, 'Koressa', 'male', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (91, 'Florentia', 'female', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (92, 'Abigale', 'male', 25, 'pass');
+insert into student (id, first_name, gender, age, result) values (93, 'Gabi', 'male', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (94, 'Nickie', 'male', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (95, 'Laney', 'female', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (96, 'Devonne', 'female', 25, 'fail');
+insert into student (id, first_name, gender, age, result) values (97, 'Concettina', 'female', 23, 'fail');
+insert into student (id, first_name, gender, age, result) values (98, 'Hugibert', 'male', 22, 'fail');
+insert into student (id, first_name, gender, age, result) values (99, 'Dorotea', 'male', 24, 'fail');
+insert into student (id, first_name, gender, age, result) values (100, 'Gerry', 'male', 25, 'fail');
+
+select * from student 
+
+copy student from 'E:\DA9\SQL\SQL_TASK_1\DA9SQLTASK1.csv' DELIMITER ',' csv header
+
+select * from student
